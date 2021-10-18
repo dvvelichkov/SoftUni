ac1817cdc19bb1c1a47cc942187b935231cd63b3
Dimitar Velichkov <91830188+dvvelichkov@users.noreply.github.com>
2021-10-18 20:53:04 +0300

    Add files via upload

---
+++ Programming Basics C#/Conditional Statements Exercise/Time + 15 Minutes/StartUp.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Time___15_Minutes
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int minutesAdded = minutes + 15;
+            if (hours < 23)
+            {
+                if (minutesAdded > 59)
+                {
+                    hours = hours + 1;
+                    minutesAdded = minutesAdded - 60;
+                    if (minutesAdded < 10)
+                    {
+                        Console.WriteLine(hours + ":" + "0" + minutesAdded);
+                    }
+                    else if (10 <= minutesAdded)
+                    {
+                        Console.WriteLine(hours + ":" + minutesAdded);
+                    }
+                }
+                else if (minutesAdded <= 59)
+                {
+                    hours = hours + 0;
+                    minutesAdded = minutesAdded + 0;
+                    if (minutesAdded < 10)
+                    {
+                        Console.WriteLine(hours + ":" + "0" + minutesAdded);
+                    }
+                    else if (10 <= minutesAdded)
+                    {
+                        Console.WriteLine(hours + ":" + minutesAdded);
+                    }
+                }
+            }
+            if (hours == 23)
+            {
+                if (minutesAdded > 59)
+                {
+                    hours = hours - 23;
+                    minutesAdded = minutesAdded - 60;
+                    if (minutesAdded < 10)
+                    {
+                        Console.WriteLine(hours + ":" + "0" + minutesAdded);
+                    }
+                    else if (10 <= minutesAdded)
+                    {
+                        Console.WriteLine(hours + ":" + minutesAdded);
+                    }
+                }
+                else if (minutesAdded <= 59)
+                {
+                    hours = hours + 0;
+                    minutesAdded = minutesAdded + 0;
+                    if (minutesAdded < 10)
+                    {
+                        Console.WriteLine(hours + ":" + "0" + minutesAdded);
+                    }
+                    else if (10 <= minutesAdded)
+                    {
+                        Console.WriteLine(hours + ":" + minutesAdded);
+                    }
+                }
+            }
+        }
+    }
+}
