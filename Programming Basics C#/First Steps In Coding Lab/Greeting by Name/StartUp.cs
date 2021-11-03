b0423c47af7ca4fa4506e260b52d44fca41c523d
Dimitar Velichkov <91830188+dvvelichkov@users.noreply.github.com>
2021-11-03 22:10:22 +0200

    Add files via upload

---
+++ Programming Basics C#/First Steps In Coding Lab/Greeting by Name/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Greeting_by_Name
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
