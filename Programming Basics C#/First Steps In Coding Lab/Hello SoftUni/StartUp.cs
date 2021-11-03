ccca295f00d9f66d791f426a4a623e594901373c
Dimitar Velichkov <91830188+dvvelichkov@users.noreply.github.com>
2021-11-03 22:11:21 +0200

    Add files via upload

---
+++ Programming Basics C#/First Steps In Coding Lab/Hello SoftUni/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_SoftUni
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
