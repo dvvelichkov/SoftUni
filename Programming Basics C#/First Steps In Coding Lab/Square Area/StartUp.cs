17354b356c648742aa07b96d3f755afd3e55d3b7
Dimitar Velichkov <91830188+dvvelichkov@users.noreply.github.com>
2021-11-03 23:08:25 +0200

    Add files via upload

---
+++ Programming Basics C#/First Steps In Coding Lab/Square Area/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Square_Area
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int squareSide = int.Parse(Console.ReadLine());
+            Console.WriteLine(squareSide * squareSide);
+        }
+    }
+}
