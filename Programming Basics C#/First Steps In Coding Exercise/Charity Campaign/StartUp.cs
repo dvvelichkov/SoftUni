ea6ebf927e6e54e958fa2aa7634c72c879e2c6a5
Dimitar Velichkov <91830188+dvvelichkov@users.noreply.github.com>
2021-11-04 00:07:04 +0200

    Add files via upload

---
+++ Programming Basics C#/First Steps In Coding Exercise/Charity Campaign/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Charity_Campaign
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int daysOfCampaign = int.Parse(Console.ReadLine());
+            int countOfBakers = int.Parse(Console.ReadLine());
+            int countOfCakes = int.Parse(Console.ReadLine());
+            int countOfWaffles = int.Parse(Console.ReadLine());
+            int countOfPancakes = int.Parse(Console.ReadLine());
+            const double pricePerCake = 45;
+            const double pricePerWaffle = 5.80;
+            const double pricePerPancake = 3.20;
+            double sumGatheredDaily = countOfBakers * (countOfCakes * pricePerCake + countOfWaffles * pricePerWaffle + countOfPancakes * pricePerPancake);
+            double endSumGathered = 7*(sumGatheredDaily * daysOfCampaign)/8;
+            Console.WriteLine(endSumGathered);
+        }
+    }
+}
