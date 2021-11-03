a76812dcb3ac50ee60834bc3807a81f5dd675ef2
Dimitar Velichkov <91830188+dvvelichkov@users.noreply.github.com>
2021-11-03 22:49:30 +0200

    Add files via upload

---
+++ Programming Basics C#/First Steps In Coding Lab/Projects Creation/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Projects_Creation
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string architectsName = Console.ReadLine();
+            int projectsCount = int.Parse(Console.ReadLine());
+            var hoursNeeded = projectsCount * 3;
+            Console.WriteLine($"The architect {architectsName} will need {hoursNeeded} hours to complete {projectsCount} project/s.");
+        }
+    }
+}
