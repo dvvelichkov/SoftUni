b18ec85fa160170be1aa4dcdac8a8fa3a83d5b45
Dimitar Velichkov <91830188+dvvelichkov@users.noreply.github.com>
2021-11-04 00:19:32 +0200

    Add files via upload

---
+++ Programming Basics C#/For Loop Exercise/Half Sum Element/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Half_Sum_Element
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int maxNum = int.MinValue;
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+
+                if (num > maxNum)
+                {
+                    maxNum = num;
+                }
+            }
+            int sumWithoutMaxNum = sum - maxNum;
+            if (maxNum == sumWithoutMaxNum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {sumWithoutMaxNum}");
+            }
+            else
+            {
+                int difference = Math.Abs(maxNum - sumWithoutMaxNum);
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {difference}");
+            }
+        }
+    }
+}
