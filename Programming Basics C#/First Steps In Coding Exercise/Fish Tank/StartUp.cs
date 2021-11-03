cfcf70f6fecc0f3cd8355b7ad9ccd54628077b2e
Dimitar Velichkov <91830188+dvvelichkov@users.noreply.github.com>
2021-11-04 00:11:21 +0200

    Add files via upload

---
+++ Programming Basics C#/First Steps In Coding Exercise/Fish Tank/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fish_Tank
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int aquariumLength = int.Parse(Console.ReadLine());
+            int aquariumWidth = int.Parse(Console.ReadLine());
+            int aquariumHeight = int.Parse(Console.ReadLine());
+            double percentage = double.Parse(Console.ReadLine());
+            double percentageConverted = percentage * 0.01;
+            double aquariumVolume = aquariumLength * aquariumWidth * aquariumHeight;
+            double aquariumVolumeToBeFilledWithWater = (aquariumVolume - (aquariumVolume * percentageConverted))*0.001;
+            Console.WriteLine(aquariumVolumeToBeFilledWithWater);
+        }
+    }
+}
+
+
