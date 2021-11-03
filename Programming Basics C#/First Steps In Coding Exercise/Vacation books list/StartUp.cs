67e3600c33dee1b1f0be4e9e759169cdccfaefd9
Dimitar Velichkov <91830188+dvvelichkov@users.noreply.github.com>
2021-11-04 00:16:31 +0200

    Add files via upload

---
+++ Programming Basics C#/First Steps In Coding Exercise/Vacation books list/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Vacation_books_list
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int pagesInCurrentBook = int.Parse(Console.ReadLine());
+            double pagesReadPerHour = double.Parse(Console.ReadLine());
+            int daysForReadingTheBook = int.Parse(Console.ReadLine());
+            double timeNeededForTheBook = pagesInCurrentBook / pagesReadPerHour;
+            double hoursPerDay = timeNeededForTheBook / daysForReadingTheBook;
+            Console.WriteLine(hoursPerDay);
+        }
+    }
+}
