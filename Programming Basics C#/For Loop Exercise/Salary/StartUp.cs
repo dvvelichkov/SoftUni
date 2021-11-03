43a825977972e44e2c46251deee0b512197c3c41
Dimitar Velichkov <91830188+dvvelichkov@users.noreply.github.com>
2021-11-04 00:24:45 +0200

    Add files via upload

---
+++ Programming Basics C#/For Loop Exercise/Salary/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Salary
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            const double facebookFine = 150;
+            const double instagramFine = 100;
+            const double redditFine = 50;
+
+            int openedTabs = int.Parse(Console.ReadLine());
+            double salary = double.Parse(Console.ReadLine());
+            string websitesNames = "";
+
+            for (int i = 0; i < openedTabs; i++)
+            {
+                websitesNames = Console.ReadLine();
+                if (websitesNames == "Facebook")
+                {
+                    salary -= facebookFine;
+                }
+                if (websitesNames == "Instagram")
+                {
+                    salary -= instagramFine;
+                }
+                if (websitesNames == "Reddit")
+                {
+                    salary -= redditFine;
+                }
+            }
+            if (salary <= 0)
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+            else
+            {
+                Console.WriteLine(salary);
+            }
+        }
+    }
+}
