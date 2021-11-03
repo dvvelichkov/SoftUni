82662912b7363435300925f540ae0bb07f8e1b9f
Dimitar Velichkov <91830188+dvvelichkov@users.noreply.github.com>
2021-11-04 00:12:23 +0200

    Add files via upload

---
+++ Programming Basics C#/First Steps In Coding Exercise/Fruit Market/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fruit_Market
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double priceOfStrawberries = double.Parse(Console.ReadLine());
+            double priceOfRaspberries = 0.5 * priceOfStrawberries;
+            double priceOfOranges = 0.6 * priceOfRaspberries;
+            double priceOfBananas = 0.2 * priceOfRaspberries;
+            double quantityOfBananas = double.Parse(Console.ReadLine());
+            double quantityOfOranges = double.Parse(Console.ReadLine());
+            double quantityOfRaspberries = double.Parse(Console.ReadLine());
+            double quantityOfStrawberries = double.Parse(Console.ReadLine());
+            double sumNeeded = priceOfStrawberries * quantityOfStrawberries + priceOfBananas * quantityOfBananas + 
+                priceOfOranges * quantityOfOranges + priceOfRaspberries * quantityOfRaspberries;
+            Console.WriteLine(sumNeeded);
+
+        }
+    }
+}
