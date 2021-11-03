ca952ce6ae62ee7b9d57cdcd771a074ce2c60c32
Dimitar Velichkov <91830188+dvvelichkov@users.noreply.github.com>
2021-11-03 22:01:02 +0200

    Add files via upload

---
+++ Programming Basics C#/Conditional Statements Lab/Password Guess/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Password_Guess
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            if (password == "s3cr3t!P@ssw0rd")
+            {
+                Console.WriteLine("Welcome");
+            }
+            else if (password != "s3cr3t!P@ssw0rd")
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
