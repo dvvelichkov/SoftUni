1796fe262038cf82b8586e02f15143ae3cea7162
Dimitar Velichkov <91830188+dvvelichkov@users.noreply.github.com>
2021-11-03 22:31:10 +0200

    Add files via upload

---
+++ Programming Basics C#/First Steps In Coding Lab/Pet Shop/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Pet_Shop
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            const double priceOfDogFood = 2.50;
+            const double priceOfOtherAnimalsFood = 4;
+            int countOfDogs = int.Parse(Console.ReadLine());
+            int countOfOtherAnimals = int.Parse(Console.ReadLine());
+            double endSum = priceOfDogFood * countOfDogs + priceOfOtherAnimalsFood * countOfOtherAnimals;
+            Console.WriteLine($"{endSum} lv.");
+        }
+    }
+}
