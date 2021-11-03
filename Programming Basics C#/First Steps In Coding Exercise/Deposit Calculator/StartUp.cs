e17844ea057e43f09a34bbca350b2bc68a4fb0e4
Dimitar Velichkov <91830188+dvvelichkov@users.noreply.github.com>
2021-11-04 00:10:07 +0200

    Add files via upload

---
+++ Programming Basics C#/First Steps In Coding Exercise/Deposit Calculator/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Deposit_Calculator
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double depositAmount = double.Parse(Console.ReadLine());
+            int depositMaturityInMonths = int.Parse(Console.ReadLine());
+            double annualInterestPercentage = double.Parse(Console.ReadLine());
+            double interestAmountperYear = (depositAmount * annualInterestPercentage)/100;
+            double interestAmountperMonth = interestAmountperYear / 12;
+            double endSum = depositAmount + (depositMaturityInMonths * interestAmountperMonth);
+            Console.WriteLine($"{endSum:f2}");
+        }
+    }
+}
