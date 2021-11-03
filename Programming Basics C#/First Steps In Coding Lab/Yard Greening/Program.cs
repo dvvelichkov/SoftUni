825589f59778adb7349864f0874d15a3d57f1cb2
Dimitar Velichkov <91830188+dvvelichkov@users.noreply.github.com>
2021-11-03 23:57:35 +0200

    Add files via upload

---
+++ Programming Basics C#/First Steps In Coding Lab/Yard Greening/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Yard_Greening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double squareMeters = double.Parse(Console.ReadLine());
+            const double pricePerSquareMeter = 7.61;
+            double finalPrice = 0.82 * squareMeters * pricePerSquareMeter;
+            double discount = 0.18 * squareMeters * pricePerSquareMeter;
+            Console.WriteLine($"The final price is: {finalPrice} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+        }
+    }
+}
